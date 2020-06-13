ea54ce321af40c8ab2b2e5667458498ecff0b4fc
manger <547192182@qq.com>
2020-06-13 15:31:23 +0800

    创建分支

--- src/main/java/com/aoligei.java
+++ src/main/java/com/aoligei.java
@@ -4,7 +4,9 @@ public class aoligei {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("hello world!");
-        System.out.println("hello world!");
+        System.out.println("21313123213123123");
+        System.out.println("1111");
+        System.out.println("1111");
         System.out.println("1111");
     }
 }
